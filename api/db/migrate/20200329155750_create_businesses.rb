8f13ac10710b9d88b2d1a64faf7aaec1357b48ce
Agustin Pirotto <agustin.pirotto@xmartlabs.com>
2020-03-29 18:03:09 -0300

    Add validation of presence in some columns of the business table

--- api/db/migrate/20200329155750_create_businesses.rb
+++ api/db/migrate/20200329155750_create_businesses.rb
@@ -1,13 +1,13 @@
 class CreateBusinesses < ActiveRecord::Migration[6.0]
   def change
     create_table :businesses do |t|
-      t.string :name
-      t.integer :type
+      t.string :name, null: false
+      t.integer :type, null: false
       t.string :phone_numbers
-      t.string :email
+      t.string :email, null: false
       t.string :instagram_url
       t.string :facebook_url
-      t.text :description
+      t.text :description, null: false
       t.boolean :verified, default: false
       t.text :tags
       t.text :availability_hours
